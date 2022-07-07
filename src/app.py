1df0462d31af97ed999915e5bb701efba5ee4ae3
jlsamo1989 <jlsamo1989@gmail.com>
2022-07-06 23:29:16 -0500

    DataEngineering: master+feature2 v1.0.1 JS

--- src/app.py
+++ src/app.py
@@ -4,4 +4,6 @@ def sum(a, b):
     return a + b
 def rest(a, b):
     return a - b #nueva funcionalidad
+def div(a, b):
+    return a / b #nueva funcionalidad
 >>>>>>> 3973208 (DataEngineering: master + feature1 v1.0.1 JS)
